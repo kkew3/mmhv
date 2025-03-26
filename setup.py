0b0654f5a94a1ac9231661dc6d430fcb64f1ac9d
kkew3 <kps6326@hotmail.com>
2025-03-26 10:03:41 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+def read_requirements():
+    with open('requirements.txt', encoding='utf-8') as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.startswith('#')
+        ]
+
+
+setup(
+    name='quick-read-miniflux-headline',
+    py_modules='app',
+    version='0.1.0',
+    install_requires=read_requirements(),
+)
